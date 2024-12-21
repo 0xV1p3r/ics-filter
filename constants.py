370491f6ae0e65e830516ca2505434a20790e1db
LightningV1p3r <contact@lightningv1p3r.net>
2024-12-21 15:03:09 +0100

    feat: Added GIT Env Vars.

--- constants.py
+++ constants.py
@@ -1,12 +1,13 @@
-REPO_NAME = "git_repo"
-SETUP_CREDENTIAL_HELPER = "git config credential.helper '!f() { sleep 1; echo \"username=${GIT_USER}\"; echo \"password=${GIT_PASSWORD}\"; }; f'"
-URL_FILE = "urls.json"
-ICS_FILE_LOCATION = "ics_files"
+BLACKLIST_EVENT_FIELD = ["created"]
 BLACKLIST_FILE = "blacklist.json"
 CONFIG_FILE = "config.ini"
-SEPARATOR_LENGTH = 30
-BLACKLIST_EVENT_FIELD = ["created"]
-DIFF_NEW = 'diff --unchanged-line-format="" --old-line-format="" --new-line-format=":%dn: %L"'
 DIFF_DEL = 'diff --unchanged-line-format="" --old-line-format=":%dn: %L" --new-line-format=""'
-OD_FILE = "/tmp/old_data.ics"
+DIFF_NEW = 'diff --unchanged-line-format="" --old-line-format="" --new-line-format=":%dn: %L"'
+GIT_USER_ENV = "GIT_USER"
+GIT_PASSWORD_ENV = "GIT_PASSWORD"
+ICS_FILE_LOCATION = "ics_files"
 ND_FILE = "/tmp/new_data.ics"
+OD_FILE = "/tmp/old_data.ics"
+REPO_LOCATION = "git_repo"
+SEPARATOR_LENGTH = 30
+URL_FILE = "urls.json"
