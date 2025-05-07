adf0fc3ec9897990e998a5b1b6ce0395858ea136
0xV1p3r <0xV1p3r@protonmail.com>
2025-05-07 18:06:56 +0200

    refactor!: made config file optional

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod version_control;
 
-use clap::Parser;
 use config::Config;
 use diff;
 use git2::Repository;
@@ -8,9 +7,7 @@ use icalendar::{Calendar, CalendarComponent, Component};
 use reqwest;
 use serde::Deserialize;
 use serde_json;
-use std::fs;
-use std::fs::read_to_string;
-use std::path::Path;
+use std::{fs, path::Path};
 
 static CALENDAR_FILE: &str = "calendars.json";
 static REPO_PATH: &str = "calendar_repo";
@@ -18,11 +15,11 @@ static SERVING_DIR: &str = "ics_files";
 
 #[derive(Debug, Deserialize)]
 struct AppConfig {
-    domain: Option<String>,
-    enable_remote: bool,
-    remote_name: Option<String>,
-    username: Option<String>,
-    token: Option<String>,
+    domain: String,
+    remote_enabled: bool,
+    remote_name: String,
+    username: String,
+    token: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -30,12 +27,6 @@ struct AppCalendar {
     blacklist: Vec<String>,
     name: String,
     url: String,
-}
-
-#[derive(Parser)]
-struct CLIArguments {
-    #[arg(long)]
-    config_file: Option<String>,
 }
 
 fn fetch_calendar(url: &String) -> String {
@@ -61,7 +52,7 @@ fn build_filtered_calendar(calendar: &AppCalendar) {
     let raw_path = format!("{}/{}.ics", REPO_PATH, calendar.name);
     let path = Path::new(&raw_path);
 
-    let data = read_to_string(path).unwrap();
+    let data = fs::read_to_string(path).unwrap();
     let parsed_calendar: Calendar = data.parse().unwrap();
 
     let mut filtered_calendar = Calendar::new();
@@ -89,16 +80,16 @@ fn load_repo(config: &AppConfig) -> Repository {
     // TODO: Better error handling (Result & anyhow)
     let repo;
     if !Path::new(REPO_PATH).exists() {
-        if !config.enable_remote {
+        if !config.remote_enabled {
             fs::create_dir(REPO_PATH).unwrap();
             repo = Repository::init(REPO_PATH).unwrap();
         } else {
             let url = format!(
                 "https://{}@{}/{}/{}.git",
-                config.token.clone().unwrap(),
-                config.domain.clone().unwrap(),
-                config.username.clone().unwrap(),
-                config.remote_name.clone().unwrap()
+                config.token.clone(),
+                config.domain.clone(),
+                config.username.clone(),
+                config.remote_name.clone()
             );
             repo = Repository::clone(&url, REPO_PATH).unwrap();
         }
@@ -108,10 +99,10 @@ fn load_repo(config: &AppConfig) -> Repository {
     repo
 }
 
-fn load_config(file: &String) -> AppConfig {
+fn load_config() -> AppConfig {
     // TODO: Better error handling (Result & anyhow)
     let config = Config::builder()
-        .add_source(config::File::with_name(&file))
+        .add_source(config::File::with_name("config.toml"))
         .build();
     if let Err(e) = config {
         panic!("{e}");
@@ -122,7 +113,7 @@ fn load_config(file: &String) -> AppConfig {
 
 fn load_calendars(file: &str) -> Vec<AppCalendar> {
     // TODO: Better error handling (Result & anyhow)
-    let raw_data = read_to_string(file).expect("Unable to open calendar file!");
+    let raw_data = fs::read_to_string(file).expect("Unable to open calendar file!");
     serde_json::from_str(&raw_data).expect("Unable to parse calendar file!")
 }
 
@@ -137,7 +128,7 @@ fn pipeline(calendar: &AppCalendar) {
         return;
     }
 
-    let local_data = read_to_string(file_path).expect("Unable to read local calendar data!");
+    let local_data = fs::read_to_string(file_path).expect("Unable to read local calendar data!");
 
     if compare_calendars(&local_data, &remote_data) {
         return;
@@ -187,20 +178,14 @@ fn commit_repo_changes(repository: &Repository) {
 }
 
 fn main() {
-    let args = CLIArguments::parse();
-    let config = if args.config_file.is_some() {
-        load_config(&args.config_file.unwrap())
-    } else {
-        AppConfig {
-            enable_remote: false,
-            domain: None,
-            remote_name: None,
-            username: None,
-            token: None,
-        }
-    };
     let calendars = load_calendars(CALENDAR_FILE);
-    let repo = load_repo(&config);
+
+    if !Path::new(REPO_PATH).exists() {
+        match fs::create_dir(REPO_PATH) {
+            Ok(_) => (),
+            Err(e) => panic!("{}", e),
+        }
+    }
 
     for calendar in calendars {
         print!("Running pipeline for '{}'...", calendar.name);
@@ -208,17 +193,23 @@ fn main() {
         print!(" done.\n");
     }
 
-    if check_repo_for_changes(&repo) {
-        print!("Committing changes...");
-        commit_repo_changes(&repo);
-        if config.enable_remote {
-            version_control::push_to_remote(
-                &repo,
-                &config.username.clone().unwrap(),
-                &config.token.clone().unwrap(),
-            );
+    if Path::new("config.toml").exists() {
+        let config = load_config();
+        let repo = load_repo(&config);
+        if check_repo_for_changes(&repo) {
+            print!("Committing changes...");
+            commit_repo_changes(&repo);
+            if config.remote_enabled {
+                version_control::push_to_remote(
+                    &repo,
+                    &config.username.clone(),
+                    &config.token.clone(),
+                );
+            }
+            refresh_serving_directory();
+            print!(" done.\n");
         }
+    } else {
         refresh_serving_directory();
-        print!(" done.\n");
     }
 }
