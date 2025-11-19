6fd1246e5f81c6ba45b103e6da199fc00e7a1d75
0xV1p3r <0xV1p3r@protonmail.com>
2025-11-19 21:13:38 +0100

    refactor(migrations): cleaned up AI artifacts

--- migrations/2025-11-19-132947-0000_create_events/up.sql
+++ migrations/2025-11-19-132947-0000_create_events/up.sql
@@ -1,5 +1,3 @@
-PRAGMA foreign_keys = ON;
-
 CREATE TABLE IF NOT EXISTS events (
     id INTEGER NOT NULL PRIMARY KEY,
     calendar_id INTEGER NOT NULL,
@@ -14,5 +12,4 @@ CREATE TABLE IF NOT EXISTS events (
     end_date DATETIME NOT NULL,
 
     FOREIGN KEY (calendar_id) REFERENCES calendars(id) ON DELETE CASCADE,
-    UNIQUE (calendar_id, uid)
 );
